164c73f4d83e9472c789633e18b5b56d9a477c49
Seth Denner <seth@knotis.com>
2015-03-25 16:05:50 -0700

    In Memory Operations (Relational Support)
    * Ported predicate.py module from https://github.com/vaterlaus/django_cassandra_backend to handle in memory sorting and filtering operations.
    * Modified compiler.py to take advantage of predicate.py in retriving and filtering results.
    * Modified settings.py to look for environment variables to configure your cassandra host and port (original defaults are still in place i.e localhost:9042)

---
+++ djangocassandra/db/backends/cassandra/exceptions.py
@@ -0,0 +1,24 @@
+from django.db import (
+    NotSupportedError
+)
+
+class InefficientQueryError(NotSupportedError):
+    message = (
+        'Innefficent queries are not allowed on this model. '
+        'Make sure if you are filtering that you are only '
+        'filtering on either primary key or indexed fields '
+        'and that if you are ordering that you are only '
+        'ordering on fields that are components of compound '
+        'primary keys.\n\nAlternatively you can enable '
+        'inefficient filtering and ordering in your database '
+        'by setting the ALLOW_INEFFICIENT_QUERIES=True in '
+        'your settings.py or you can set this per model in '
+        'the CassandraMeta class for you model '
+        '"allow_inefficient_queries=True".'
+    )
+
+    def __init__(self, query):
+        self.query = query
+
+    def __str__(self):
+        return  (self.message + ':\n%s') % (repr(self.query),)
--- tests/settings.py
+++ tests/settings.py
@@ -1,14 +1,24 @@
+import os
+import uuid
+
 from cassandra.metadata import (
     SimpleStrategy
 )
-SECRET_KEY = "foo"
+
+SECRET_KEY = uuid.uuid4().hex
 
 DATABASES = {
     'default': {
         'ENGINE': 'djangocassandra.db.backends.cassandra',
         'DEFAULT_KEYSPACE': 'test',
-        'CONTACT_POINTS': ('localhost',),
-        'PORT': 9042,
+        'CONTACT_POINTS': (os.environ.get(
+            'DJANGOCASSANDRA_TEST_HOST',
+            'localhost'
+        ),),
+        'PORT': int(os.environ.get(
+            'DJANGOCASSANDRA_TEST_PORT',
+            9042
+        )),
         'KEYSPACES': {
             'test': {
                 'replication_factor': 1,
