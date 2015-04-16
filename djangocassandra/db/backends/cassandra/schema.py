bf35ff31c7a56870e1f79602a95a4b404f92617f
Seth Denner <seth@knotis.com>
2015-04-16 14:40:39 -0700

    Release Version 0.0.6 Alpha
    * Implemented Update call that I managed to get this far without running into yet. Seems to work good now.
    * Fixed some issues with add/remove fields in the SchemaEditor class. The column family class wasn't getting all of it's fields updated. I should probably be using the metaclass to generate a new class that has the appropriate values but this workaround of assiging the columns attribute directly seems to work fine.
    * Enabled allow filtering by default on queries. I should add an option for this.
    * Added a attribution to the quote on index.rst.
    * Bumped Version to 0.0.6

--- djangocassandra/db/backends/cassandra/schema.py
+++ djangocassandra/db/backends/cassandra/schema.py
@@ -5,6 +5,7 @@ from django.db.backends.schema import (
 )
 
 from djangocassandra.db.models import (
+    CqlColumnFamilyMetaClass,
     get_column_family,
     internal_type_to_column_map
 )
@@ -93,7 +94,13 @@ class CassandraSchemaEditor(BaseDatabaseSchemaEditor):
                (disable reads on OLD_Keyspace)
             7) clean up OLD_Keyspace (drop schema, delete files, etc.)
         '''
-        pass
+        if (
+            new_db_table._meta.db_table !=
+            old_db_table._meta.db_table
+        ):
+            raise Exception('Renaming models not supported')
+
+        self.create_model(new_db_table)
 
     def alter_db_tablespace(
         self,
@@ -132,14 +139,22 @@ class CassandraSchemaEditor(BaseDatabaseSchemaEditor):
                 model
             )
 
+        db_field = (
+            field.db_column
+            if field.db_column
+            else field.column
+        )            
         cql_field_type = internal_type_to_column_map[field.get_internal_type()]
         cql_field = cql_field_type(
             primary_key=field.primary_key,
             index=field.db_index,
-            db_field=field.name,
+            db_field=db_field,
             required=not field.blank
         )
-        setattr(column_family, field.name, cql_field)
+        setattr(column_family, db_field, cql_field)
+        column_family._columns[db_field] = cql_field
+
+        CqlColumnFamilyMetaClass.update_column_family(column_family)
         return column_family
         
     def remove_field(
@@ -165,7 +180,16 @@ class CassandraSchemaEditor(BaseDatabaseSchemaEditor):
                 model
             )
 
+        
         delattr(column_family, field.name)
+        db_field = (
+            field.db_column
+            if field.db_column
+            else field.column
+        )            
+        del column_family._columns[db_field]
+
+        CqlColumnFamilyMetaClass.update_column_family(column_family)
         return column_family
 
     def alter_field(
