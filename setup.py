3174335f54406d23aa910eafb302b38841870805
Seth Denner <seth@knotis.com>
2015-10-27 14:18:11 -0700

    Incrementing version number because I accidentally distributed the wrong version to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='djangocassandra',
-    version='0.1.9',
+    version='0.2.0',
     description='Cassandra support for the Django web framework',
     long_description=(
         'The Cassandra database backend for Django has been '
